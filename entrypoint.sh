d092163a9cc0e16cc39015601306711434898786
dylanmcauliffe <50005666+dylanmcauliffe@users.noreply.github.com>
2019-05-31 13:57:17 -0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,29 @@
-#!/bin/bash
+#!/bin/sh
 
 # exit on error
 set -e
 
 echo ${DATA}
+
+# initialize data directory
+init_data() {
+	# create root directory
+	mkdir -p ${DATA}
+	
+	# if not directory /etc then create
+	if [[ ! -d ${DATA}/etc ]];
+	then
+		mv /etc/bind ${DATA}/etc
+	fi
+	
+	# delete old location
+	rm -rf /etc/bind
+	
+	# link old location to new directory
+	ln -sf ${DATA}/etc /etc/bind
+}
+
+init_data
 
 # run CMD
 exec "$@"
